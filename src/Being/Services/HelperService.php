206a7258bb0551c3f15e8a523463fbde10eaaf04
Yaoguais <newtopstdio@163.com>
2016-12-29 15:34:16 +0800

    add a demo

---
+++ src/Being/Services/HelperService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Being\Services;
+
+class HelperService
+{
+    /**
+     * Format the limit param for preventing large data request destroy
+     * This is for laravel framework
+     * @param $request
+     * @param int $default
+     * @param int $max
+     * @param string $key
+     * @return int
+     */
+    public static function limit($request, $default = 10, $max = 100, $key = 'limit')
+    {
+        $limit = $request->input($key);
+        if (is_null($limit)) {
+            return $default;
+        }
+        $max = max($default, $max);
+        $ret = $limit > $max ? $max : ($limit < 0 ? 0 : intval($limit));
+
+        return $ret;
+    }
+}
