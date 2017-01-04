0226ae5ddb61ce0edc37babe4b58d59ace1c3adb
Yaoguais <newtopstdio@163.com>
2017-01-04 10:57:43 +0800

    change anonymous class to normal class

--- tests/Being/Services/App/AppServiceTest.php
+++ tests/Being/Services/App/AppServiceTest.php
@@ -11,23 +11,25 @@ class AppServiceTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->request = new class
-        {
-            public function input($key = null, $default = null)
-            {
-                $params = [
-                    'limit' => 30,
-                    'user_limit' => 120,
-                ];
-
-                return is_null($key) ? $params : (array_key_exists($key, $params) ? $params[$key] : $default);
-            }
-        };
+        $this->request = new Request();
     }
 
     public function testLimit()
     {
         $this->assertTrue(AppService::limit($this->request) === 30);
         $this->assertTrue(AppService::limit($this->request, 10, 100, 'user_limit') === 100);
+    }
+}
+
+class Request
+{
+    public function input($key = null, $default = null)
+    {
+        $params = [
+            'limit' => 30,
+            'user_limit' => 120,
+        ];
+
+        return is_null($key) ? $params : (array_key_exists($key, $params) ? $params[$key] : $default);
     }
 }
