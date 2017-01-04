c0974228a86f764e204b7d60738d3f05e66ca5c8
Yaoguais <newtopstdio@163.com>
2017-01-04 10:28:02 +0800

    add test case of AppService::limit

--- tests/Being/Services/App/AppServiceTest.php
+++ tests/Being/Services/App/AppServiceTest.php
@@ -2,12 +2,32 @@
 
 namespace Tests\Being\Services\App;
 
+use Being\Services\App\AppService;
 use PHPUnit_Framework_TestCase;
 
 class AppServiceTest extends PHPUnit_Framework_TestCase
 {
+    protected $request;
+
+    public function setUp()
+    {
+        $this->request = new class
+        {
+            public function input($key = null, $default = null)
+            {
+                $params = [
+                    'limit' => 30,
+                    'user_limit' => 120,
+                ];
+
+                return is_null($key) ? $params : (array_key_exists($key, $params) ? $params[$key] : $default);
+            }
+        };
+    }
+
     public function testLimit()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(AppService::limit($this->request) === 30);
+        $this->assertTrue(AppService::limit($this->request, 10, 100, 'user_limit') === 100);
     }
 }
