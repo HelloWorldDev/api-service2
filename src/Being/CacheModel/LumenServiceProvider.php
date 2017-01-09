3ff2b3c8ce533db7e758fcbfd7c423cb6122752c
Yaoguais <newtopstdio@163.com>
2017-01-09 11:14:30 +0800

    use singleton class RedisCache

--- src/Being/CacheModel/LumenServiceProvider.php
+++ src/Being/CacheModel/LumenServiceProvider.php
@@ -2,36 +2,12 @@
 
 namespace Being\CacheModel;
 
-use Angejia\Pea\Cache;
-use Angejia\Pea\Meta;
-use Angejia\Pea\RedisMeta;
-use Angejia\Pea\RedisCache;
 use Angejia\Pea\SchemaFacade;
 
-class LumenServiceProvider extends \Illuminate\Support\ServiceProvider
+class LumenServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         class_alias(SchemaFacade::class, 'Schema');
-    }
-
-    public function register()
-    {
-        // TODO 缓存 redis 实例可以配置
-        $this->app->singleton(Meta::class, function () {
-            return new RedisMeta($this->getRedis());
-        });
-        $this->app->singleton(Cache::class, function () {
-            return new RedisCache($this->getRedis());
-        });
-    }
-
-    /**
-     * 默认取名称为 pea 的 Redis 实例；如果没有，则取 default 。
-     */
-    private function getRedis()
-    {
-        $redis = $this->app->make('redis');
-        return $redis->connection('pea') ?: $redis->connection();
     }
 }
--- src/Being/CacheModel/ServiceProvider.php
+++ src/Being/CacheModel/ServiceProvider.php
@@ -7,6 +7,8 @@ use Angejia\Pea\Meta;
 use Angejia\Pea\RedisMeta;
 use Angejia\Pea\RedisCache;
 use Angejia\Pea\SchemaFacade;
+use Illuminate\Redis\Database;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Foundation\AliasLoader;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -19,21 +21,19 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function register()
     {
-        // TODO 缓存 redis 实例可以配置
+        $this->app->singleton(RedisCache::class, function () {
+            $config = config('database.redis_cache');
+            if (!empty($config)) {
+                return (new Database($config))->connection();
+            } else {
+                return Redis::connection('pea') ? : Redis::connection();
+            }
+        });
         $this->app->singleton(Meta::class, function () {
-            return new RedisMeta($this->getRedis());
+            return new RedisMeta(app(RedisCache::class));
         });
         $this->app->singleton(Cache::class, function () {
-            return new RedisCache($this->getRedis());
+            return new RedisCache(app(RedisCache::class));
         });
-    }
-
-    /**
-     * 默认取名称为 pea 的 Redis 实例；如果没有，则取 default 。
-     */
-    private function getRedis()
-    {
-        $redis = $this->app->make('redis');
-        return $redis->connection('pea') ?: $redis->connection();
     }
 }
