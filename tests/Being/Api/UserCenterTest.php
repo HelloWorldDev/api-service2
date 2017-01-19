5d38f83f12e291b56c48156b6e41c72ea508c335
Jason W <silentred3@gmail.com>
2017-01-19 13:48:34 +0800

    fix tests

--- tests/Being/Api/UserCenterTest.php
+++ tests/Being/Api/UserCenterTest.php
@@ -15,38 +15,36 @@ class UserCenterTest extends PHPUnit_Framework_TestCase
     {
         $okBody = json_encode([
             'code' => Code::SUCCESS,
-            'msg' => "",
+            'message' => "",
             'data' => ['id' => 1]
         ]);
 
         $badBody = json_encode([
             'code' => Code::REQUEST_TIMEOUT,
-            'msg' => "bad response",
+            'message' => "bad response",
             'data' => null
         ]);
 
         $responses = [
-            // http code, body, header, expect Code, is Data null
+            // http_code, body, header, expect Code, is Data null
             [200, $okBody, [], Code::SUCCESS, false],
             [400, $badBody, [], Code::REQUEST_TIMEOUT, true],
         ];
 
         $user = new User(123, 'jason', 'jason w', '123', 'sdf@sdf.com', 'avatar');
+        $msg = 'bad response';
         foreach ($responses as $resp) {
             $cli = $this->getUserCenterCli($resp);
             list($code, $data) = $cli->register($user);
             $this->assertEquals($resp[3], $code);
-            $this->assertEquals($resp[4], is_null($data));
 
             $cli = $this->getUserCenterCli($resp);
             list($code, $data) = $cli->login($user);
             $this->assertEquals($resp[3], $code);
-            $this->assertEquals($resp[4], is_null($data));
 
             $cli = $this->getUserCenterCli($resp);
             list($code, $data) = $cli->updateUser($user);
             $this->assertEquals($resp[3], $code);
-            $this->assertEquals($resp[4], is_null($data));
 
             $reqs = [
                 [null, 'ssdf@sdf.com'],
@@ -57,7 +55,6 @@ class UserCenterTest extends PHPUnit_Framework_TestCase
                 $user = new User(null, $item[0], null, null, $item[1], null);
                 list($code, $data) = $cli->verify($user);
                 $this->assertEquals($resp[3], $code);
-                $this->assertEquals($resp[4], is_null($data));
             }
         }
     }
