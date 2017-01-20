5e97f536618fe70df6d7576dffd7c58714f78426
zhangji <zhangji@nihao.com>
2017-01-20 16:46:47 +0800

    +weibo登录接口

--- src/Being/Api/Service/Thirdparty/AuthWeibo.php
+++ src/Being/Api/Service/Thirdparty/AuthWeibo.php
@@ -1,19 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lalala
- * Date: 2017/1/20
- * Time: 下午3:45
- */
 
 namespace Being\Api\Service\Thirdparty;
 
+use Being\WeiboOpenApi\WeiboClient;
+use Log;
 
 class AuthWeibo extends Auth
 {
-
     public function login($unionid, $code)
     {
-        // TODO: Implement login() method.
+        $client = app(WeiboClient::class);
+        $client->setAccessToken($code);
+        $userInfo = $client->show_user_by_id($unionid);
+        Log::debug(sprintf('%s:%d weibo response:%s', __FILE__, __LINE__, json_encode($userInfo)));
+        $nickname = empty($userInfo['name']) ? '' : $userInfo['name'];
+        $avatar = empty($userInfo['avatar_large']) ? '' : $userInfo['avatar_large'];
+
+        return ['nickname' => $nickname, 'avatar' => $avatar];
     }
 }
