d9b3443ff3eae388a987d192b79d5a46e22d7a1f
Yaoguais <newtopstdio@163.com>
2017-02-14 15:09:58 +0800

    submit html

--- src/Omnipay/JDPay/Message/PurchaseRequest.php
+++ src/Omnipay/JDPay/Message/PurchaseRequest.php
@@ -86,6 +86,57 @@ class PurchaseRequest extends BaseAbstractRequest
         return $this->response = null;
     }
 
+    public function getSubmitHtml()
+    {
+        $payUrl = $this->getEndpoint();
+        $data = $this->getData();
+
+        return <<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="expires" content="0"/>
+    <meta http-equiv="pragma" content="no-cache"/>
+    <meta http-equiv="cache-control" content="no-cache"/>
+    <title>Trade Submit</title>
+</head>
+<body onload="autosubmit()">
+<form action="{$payUrl}"  method="post" id="batchForm" style="visibility: hidden" >
+    <input type="text" name="version" value="{$data['version']}"/><br/>
+    <input type="text" name="merchant" value="{$data['merchant']}"/><br/>
+    <input type="text" name="device" value="{$data['device']}"/><br/>
+    <input type="text" name="tradeNum" value="{$data['tradeNum']}"/><br/>
+    <input type="text" name="tradeName" value="{$data['tradeName']}"/><br/>
+    <input type="text" name="tradeDesc" value="{$data['tradeDesc']}"/><br/>
+    <input type="text" name="tradeTime" value="{$data['tradeTime']}"/><br/>
+    <input type="text" name="amount" value="{$data['amount']}"/><br/>
+    <input type="text" name="currency" value="{$data['currency']}"/><br/>
+    <input type="text" name="note" value="{$data['note']}"/><br/>
+    <input type="text" name="callbackUrl" value="{$data['callbackUrl']}"/><br/>
+    <input type="text" name="notifyUrl" value="{$data['notifyUrl']}"/><br/>
+    <input type="text" name="ip" value="{$data['ip']}"/><br/>
+    <input type="text" name="userType" value="{$data['userType']}"/><br/>
+    <input type="text" name="userId" value="{$data['userId']}"/><br/>
+    <input type="text" name="expireTime" value="{$data['expireTime']}"/><br/>
+    <input type="text" name="orderType" value="{$data['orderType']}"/><br/>
+    <input type="text" name="industryCategoryCode" value="{$data['industryCategoryCode']}"/><br/>
+    <input type="text" name="specCardNo" value="{$data['specCardNo']}"/><br/>
+    <input type="text" name="specId" value="{$data['specId']}"/><br/>
+    <input type="text" name="specName" value="{$data['specName']}"/><br/>
+    <input type="text" name="sign" value="{$data['sign']}"/><br/>
+</form>
+<script>
+    function autosubmit(){
+        document.getElementById("batchForm").submit();
+    }
+</script>
+
+</body>
+</html>
+EOF;
+    }
+
     /**
      * @return mixed
      */
