23d91f42bad8854e0c2ea264d464752ad818bddd
Yaoguais <newtopstdio@163.com>
2017-02-17 16:28:03 +0800

    add send sms service

--- src/Being/Services/App/SmsService.php
+++ src/Being/Services/App/SmsService.php
@@ -5,9 +5,14 @@ namespace Being\Services\App;
 use Exception;
 use GuzzleHttp\Client;
 
-
 class SmsService
 {
+    /**
+     * Send Sms Message
+     * @param $mobile
+     * @param $message
+     * @return bool
+     */
     public static function send($mobile, $message)
     {
         $smsApiKey = config('app.sms_api_key');
@@ -25,13 +30,12 @@ class SmsService
                 'body' => http_build_query($data),
                 'timeout' => 5,
             ]);
-            $ret = $r->getStatusCode() == 200;
 
-            AppService::debug(sprintf('send sms to %s with message %s failed ret %d', $mobile, $message, $ret),
+            AppService::debug(sprintf('send sms to %s with message %s failed ret %d body %s',
+                $mobile, $message, $r->getStatusCode() == 200, $r->getBody()),
                 __FILE__, __LINE__);
-            if ($ret) {
-                return $r->getBody();
-            }
+
+            return $r->getStatusCode() == 200;
         } catch (Exception $e) {
             AppService::error(sprintf('send sms to %s with message %s failed err %s', $mobile, $message, $e->getMessage()),
                 __FILE__, __LINE__);
