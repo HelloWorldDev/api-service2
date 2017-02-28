24680dee1db68c3628074a2b66eadfe736caaf3f
Yaoguais <newtopstdio@163.com>
2017-02-28 17:25:39 +0800

    fix push

--- src/Being/Push/Message/ApnsMessage.php
+++ src/Being/Push/Message/ApnsMessage.php
@@ -2,38 +2,23 @@
 
 namespace Being\Push\Message;
 
-use ApnsPHP_Message;
-use ApnsPHP_Push;
-use Being\Push\Message;
+use Being\Push\Message as BaseMessage;
 use Being\Services\App\AppService;
+use Sly\NotificationPusher\PushManager,
+    Sly\NotificationPusher\Adapter\Apns as ApnsAdapter,
+    Sly\NotificationPusher\Collection\DeviceCollection,
+    Sly\NotificationPusher\Model\Device,
+    Sly\NotificationPusher\Model\Message,
+    Sly\NotificationPusher\Model\Push;
 
-class ApnsMessage extends Message
+class ApnsMessage extends BaseMessage
 {
     protected $certificateFile;
     protected $env;
-    /**
-     * @var ApnsPHP_Message
-     */
-    protected $message;
 
     public function __construct($to, $title)
     {
         parent::__construct($to, $title);
-    }
-
-    public function buildMessage()
-    {
-        $message = new ApnsPHP_Message($this->to);
-        // $message->setCustomIdentifier("Message-Badge-3");
-        $message->setBadge(1);
-        $message->setText($this->title);
-        $message->setSound();
-        // $message->setCustomProperty('acme2', array('bang', 'whiz'));
-        // $message->setCustomProperty('acme3', array('bing', 'bong'));
-        $message->setExpiry(7 * 86400);
-        $this->message = $message;
-
-        return $this;
     }
 
     public function setCertificateFile($certificateFile)
@@ -43,61 +28,39 @@ class ApnsMessage extends Message
         return $this;
     }
 
-    public function setBadge($badge)
-    {
-        $this->message->setBadge($badge);
-
-        return $this;
-    }
-
-    public function setCustomIdentifier($customIdentifier)
-    {
-        $this->message->setCustomIdentifier($customIdentifier);
-
-        return $this;
-    }
-
-    public function setCustomProperty($sName, $mValue)
-    {
-        $this->message->setCustomProperty($sName, $mValue);
-
-        return $this;
-    }
-
-    public function setExpiry($second)
-    {
-        $this->message->setExpiry($second);
-
-        return $this;
-    }
-
-
     public function send()
     {
-        if (is_null($this->message)) {
-            $this->buildMessage();
+        $env = $this->env == self::ENVIRONMENT_SANDBOX ? PushManager::ENVIRONMENT_DEV : PushManager::ENVIRONMENT_PROD;
+        $pushManager = new PushManager($env);
+        $apnsAdapter = new ApnsAdapter([
+            'certificate' => $this->certificateFile,
+        ]);
+        $devices = new DeviceCollection(array(
+            new Device($this->to),
+        ));
+        $message = new Message($this->title);
+        $push = new Push($apnsAdapter, $devices, $message);
+        $pushManager->add($push);
+
+        $ret = false;
+        $error = null;
+
+        try{
+            $pushManager->push();
+            $ret = $push->isPushed();
+        }catch(\Exception $e) {
+            $error = $e->getMessage();
         }
-        $push = new ApnsPHP_Push($this->env, $this->certificateFile);
-        // Instantiate a new ApnsPHP_Push object
-        // $this->push->setRootCertificationAuthority('entrust_root_certification_authority.pem');
-        $push->connect();
-        $push->add($this->message);
-        $push->send();
-        $push->disconnect();
-        $errors = $push->getErrors();
-        $ret = count($errors) == 0;
 
         // log
         if (class_exists('\Being\Services\App\AppService')) {
             $pushInfo = [
-                $this->env == self::ENVIRONMENT_SANDBOX ? 'sandbox' : 'production',
-                'certificateFile' => $this->certificateFile
+                'env' => $this->env == self::ENVIRONMENT_SANDBOX ? 'dev' : 'prod',
+                'to' => $this->to,
+                'ret' => $ret,
+                'error' => $error,
             ];
-            $message = sprintf('push:%s message:%s errors:%s',
-                json_encode($pushInfo),
-                $this->message->__toString(),
-                json_encode($errors)
-            );
+            $message = sprintf('push:%s', json_encode($pushInfo));
             if ($ret) {
                 \Being\Services\App\AppService::debug($message, __FILE__, __LINE__);
             } else {
