01bb95c52bb8d286c943d631b64142a0cad036ab
Yaoguais <newtopstdio@163.com>
2017-03-14 17:19:25 +0800

    fix ssl error

--- src/Omnipay/JDFastPay/PayService.php
+++ src/Omnipay/JDFastPay/PayService.php
@@ -29,7 +29,8 @@ class PayService
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_PORT, 443);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        // fix prod server ssl error
+        // curl_setopt($ch, CURLOPT_SSLVERSION, 3);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
