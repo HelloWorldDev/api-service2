ae304b0f2eb5cc6905550d76fce9b01a4796bac3
qinxiuchen <qinxiuchen@gmail.com>
2017-06-22 15:21:17 +0800

    add Facebook support
    add Facebook support

--- src/Being/Api/Service/Thirdparty/AuthFacebook.php
+++ src/Being/Api/Service/Thirdparty/AuthFacebook.php
@@ -2,15 +2,45 @@
 
 namespace Being\Api\Service\Thirdparty;
 
+
+
+use Being\Services\App\AppService;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Pheanstalk\Exception;
+
 class AuthFacebook extends Auth
 {
+    private $appId;
+    private $secret;
+
     public function setConfig($config)
     {
+        $this->appId = $config['facebook']['app_id'];
+        $this->secret = $config['facebook']['secret'];
         return $this;
     }
 
-    public function login($unionid, $code)
+    public function login($unionid, $code=0)
     {
-        return ['unionid' => $unionid, 'code' => $code];
+        $fb = new Facebook([
+            'app_id' => $this->appId,
+            'app_secret' => $this->secret,
+            'default_graph_version' => 'v2.9',
+        ]);
+
+        try {
+            //$response = $fb->get('/me?fields=id,name,first_name,last_name,picture,email', $unionid);
+            $response = $fb->get('/me?fields=id,name,picture', $unionid);
+        } catch(FacebookResponseException $e) {
+            AppService::error('Graph returned an error: ' . $e->getMessage(), __FILE__, __FILE__);
+            return [$e->getCode(), $e->getMessage()];
+        }
+        $me = $response->getGraphUser();
+        $unionid = $me->getId();
+        $avatar = $me->getPicture()->getUrl();
+        $nickname = $me->getName();
+        $code = 0;
+        return ['unionid' => $unionid, 'code' => $code, 'avatar' => $avatar, 'nickname' => $nickname];
     }
 }
